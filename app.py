22da38a83f1d812994cc4ff0688f670a073793e4
SHANU SHAH <66065871+iamshanu14@users.noreply.github.com>
2024-05-04 02:39:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,7 @@ import plotly.graph_objects as go
 from sklearn.preprocessing import MinMaxScaler
 from GoogleNews import GoogleNews
 from textblob import TextBlob
-
-# Load the pre-trained model
 model = load_model('Stock Predictions Model.keras')
-
-# Function to fetch all stock symbols and names
-def fetch_all_stocks():
-    all_stocks = yf.Tickers('')
-    symbols = []
-    names = []
-
-    for stock in all_stocks.tickers:
-        symbols.append(stock.info['symbol'])
-        names.append(stock.info['longName'])
-
-    return symbols, names
-
-symbols, names = fetch_all_stocks()
 
 st.header('Stock Market Predictor')
 
@@ -32,121 +16,109 @@ option = st.selectbox(
     ('Stock Predictions', 'Sentimental Analysis'))
 
 if option == 'Stock Predictions':
-    stock_input = st.text_input('Enter Stock Symbol or Name', '').upper()
+    stock = st.text_input('Enter Stock Symbol', 'AAPL')
 
-    if stock_input:
-        # Filter suggestions based on user input
-        suggestions = [item for item in symbols + names if stock_input in item.upper()]
-
-        if len(suggestions) > 0:
-            st.write("Suggestions:", suggestions)
-        else:
-            st.write("No suggestions found.")
-
+    
     start_date = st.date_input('Start Date', pd.Timestamp('2023-01-01'))
     end_date = st.date_input('End Date', pd.Timestamp('2024-04-30'))
 
-    if stock_input:
-        data = yf.download(stock_input, start_date, end_date)
+    data = yf.download(stock, start_date, end_date)
 
-        st.subheader('Stock Data')
-        st.write(data)
+    st.subheader('Stock Data')
+    st.write(data)
 
-        data_train = pd.DataFrame(data.Close[0: int(len(data)*0.80)])
-        data_test = pd.DataFrame(data.Close[int(len(data)*0.80): len(data)])
+    data_train = pd.DataFrame(data.Close[0: int(len(data)*0.80)])
+    data_test = pd.DataFrame(data.Close[int(len(data)*0.80): len(data)])
 
-        scaler = MinMaxScaler(feature_range=(0,1))
-        pas_100_days = data_train.tail(100)
-        data_test = pd.concat([pas_100_days, data_test], ignore_index=True)
-        data_test_scale = scaler.fit_transform(data_test)
+    scaler = MinMaxScaler(feature_range=(0,1))
+    pas_100_days = data_train.tail(100)
+    data_test = pd.concat([pas_100_days, data_test], ignore_index=True)
+    data_test_scale = scaler.fit_transform(data_test)
 
-        st.subheader('Price vs MA50')
-        ma_50_days = data.Close.rolling(50).mean()
+    st.subheader('Price vs MA50')
+    ma_50_days = data.Close.rolling(50).mean()
 
-        fig1 = go.Figure()
-        fig1.add_trace(go.Scatter(x=data.index, y=ma_50_days, mode='lines', name='MA50', line=dict(color='red')))
-        fig1.add_trace(go.Scatter(x=data.index, y=data.Close, mode='lines', name='Close', line=dict(color='green')))
-        fig1.update_layout(title='Price vs MA50', xaxis_title='Date', yaxis_title='Price')
-        st.plotly_chart(fig1)
+    fig1 = go.Figure()
+    fig1.add_trace(go.Scatter(x=data.index, y=ma_50_days, mode='lines', name='MA50', line=dict(color='red')))
+    fig1.add_trace(go.Scatter(x=data.index, y=data.Close, mode='lines', name='Close', line=dict(color='green')))
+    fig1.update_layout(title='Price vs MA50', xaxis_title='Date', yaxis_title='Price')
+    st.plotly_chart(fig1)
 
-        ma_100_days = data.Close.rolling(100).mean()
+    ma_100_days = data.Close.rolling(100).mean()
+    
+    # Plotting MA50 vs MA100
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=data.index, y=ma_50_days, mode='lines', name='MA50', line=dict(color='red')))
+    fig2.add_trace(go.Scatter(x=data.index, y=ma_100_days, mode='lines', name='MA100', line=dict(color='blue')))
+    fig2.update_layout(title='MA50 vs MA100', xaxis_title='Date', yaxis_title='Price')
+    st.plotly_chart(fig2)
 
-        # Plotting MA50 vs MA100
-        fig2 = go.Figure()
-        fig2.add_trace(go.Scatter(x=data.index, y=ma_50_days, mode='lines', name='MA50', line=dict(color='red')))
-        fig2.add_trace(go.Scatter(x=data.index, y=ma_100_days, mode='lines', name='MA100', line=dict(color='blue')))
-        fig2.update_layout(title='MA50 vs MA100', xaxis_title='Date', yaxis_title='Price')
-        st.plotly_chart(fig2)
+    ma_200_days = data.Close.rolling(200).mean()
+    # Plotting MA100 vs MA200
+    fig3 = go.Figure()
+    fig3.add_trace(go.Scatter(x=data.index, y=ma_100_days, mode='lines', name='MA100', line=dict(color='blue')))
+    fig3.add_trace(go.Scatter(x=data.index, y=ma_200_days, mode='lines', name='MA200', line=dict(color='orange')))
+    fig3.update_layout(title='MA100 vs MA200', xaxis_title='Date', yaxis_title='Price')
+    st.plotly_chart(fig3)
 
-        ma_200_days = data.Close.rolling(200).mean()
-        # Plotting MA100 vs MA200
-        fig3 = go.Figure()
-        fig3.add_trace(go.Scatter(x=data.index, y=ma_100_days, mode='lines', name='MA100', line=dict(color='blue')))
-        fig3.add_trace(go.Scatter(x=data.index, y=ma_200_days, mode='lines', name='MA200', line=dict(color='orange')))
-        fig3.update_layout(title='MA100 vs MA200', xaxis_title='Date', yaxis_title='Price')
-        st.plotly_chart(fig3)
+        
 
-        x = []
-        y = []
 
-        for i in range(100, data_test_scale.shape[0]):
-            x.append(data_test_scale[i-100:i])
-            y.append(data_test_scale[i,0])
+    x = []
+    y = []
 
-        x,y = np.array(x), np.array(y)
+    for i in range(100, data_test_scale.shape[0]):
+        x.append(data_test_scale[i-100:i])
+        y.append(data_test_scale[i,0])
 
-        predict = model.predict(x)
+    x,y = np.array(x), np.array(y)
 
-        scale = 1/scaler.scale_
+    predict = model.predict(x)
 
-        predict = predict * scale
-        y = y * scale
+    scale = 1/scaler.scale_
 
-        fig4 = go.Figure()
-        fig4.add_trace(go.Scatter(x=np.arange(len(predict)), y=predict[:,0], mode='lines', name='Original Price', line=dict(color='red')))
-        fig4.add_trace(go.Scatter(x=np.arange(len(y)), y=y, mode='lines', name='Predicted Price', line=dict(color='green')))
-        fig4.update_layout(title='Original Price vs Predicted Price', xaxis_title='Time', yaxis_title='Price')
-        st.plotly_chart(fig4)
+    predict = predict * scale
+    y = y * scale
+
+    
+    fig4 = go.Figure()
+    fig4.add_trace(go.Scatter(x=np.arange(len(predict)), y=predict[:,0], mode='lines', name='Original Price', line=dict(color='red')))
+    fig4.add_trace(go.Scatter(x=np.arange(len(y)), y=y, mode='lines', name='Predicted Price', line=dict(color='green')))
+    fig4.update_layout(title='Original Price vs Predicted Price', xaxis_title='Time', yaxis_title='Price')
+    st.plotly_chart(fig4)
 
 elif option == 'Sentimental Analysis':
-    stock_input = st.text_input('Enter Your Stock', '').upper()
+    st.title('Sentimental Analysis')
+    stock = st.text_input('Enter Your Stock', 'AAPL')
 
-    if stock_input:
-        # Filter suggestions based on user input
-        suggestions = [item for item in symbols + names if stock_input in item.upper()]
-
-        if len(suggestions) > 0:
-            st.write("Suggestions:", suggestions)
-        else:
-            st.write("No suggestions found.")
 
     start_date = st.date_input('Start Date', pd.to_datetime('2023-01-01'))
     end_date = st.date_input('End Date', pd.to_datetime('2024-04-30'))
 
-    if stock_input:
-        googlenews = GoogleNews()
-        googlenews.search(stock_input)
-        result = googlenews.result()
-        news_text = ''
-        for res in result:
-            news_text += res['title'] + '\n'
-        st.subheader('News')
-        st.write(news_text)
-        blob = TextBlob(news_text)
-        sentiment_score = blob.sentiment.polarity
-        st.subheader('Sentiment Analysis Result')
-        if sentiment_score > 0:
-            st.write('Overall sentiment: Positive')
-            st.write('Verdict: Investable')
-        elif sentiment_score < 0:
-            st.write('Overall sentiment: Negative')
-            st.write('Verdict: Not Investable')
-        else:
-            st.write('Overall sentiment: Neutral')
-            st.write('Verdict: Neutral')
+    googlenews = GoogleNews()
+    googlenews.search(stock)
+    result = googlenews.result()
+    news_text = ''
+    for res in result:
+        news_text += res['title'] + '\n'
+    st.subheader('News')
+    st.write(news_text)
+    blob = TextBlob(news_text)
+    sentiment_score = blob.sentiment.polarity
+    st.subheader('Sentiment Analysis Result')
+    if sentiment_score > 0:
+        st.write('Overall sentiment: Positive')
+        st.write('Verdict: Investable')
+    elif sentiment_score < 0:
+        st.write('Overall sentiment: Negative')
+        st.write('Verdict: Not Investable')
+    else:
+        st.write('Overall sentiment: Neutral')
+        st.write('Verdict: Neutral')
 
-        data = yf.download(stock_input, start_date, end_date)
-        fig_stock = go.Figure()
-        fig_stock.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Stock Price'))
-        fig_stock.update_layout(title='Stock Price Movement', xaxis_title='Date', yaxis_title='Price')
-        st.plotly_chart(fig_stock)
+   
+    data = yf.download(stock, start_date, end_date)
+    fig_stock = go.Figure()
+    fig_stock.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Stock Price'))
+    fig_stock.update_layout(title='Stock Price Movement', xaxis_title='Date', yaxis_title='Price')
+    st.plotly_chart(fig_stock)
